aa11107eb79401cc859df7899fe4cbc87d088d6d
BlueDev8 <65924485+dfunani@users.noreply.github.com>
2022-08-16 13:09:09 +0200

    Final changes to the game

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
 let card_deck = document.querySelector('#card-deck')
 let game_sum = document.querySelector('#game-sum')
-let dealer_sum = 0;
 let display_message = document.querySelector('#display-message')
 let card = 0;
 let message = '';
 let gameOver = false;
 document.getElementById('restart-btn').style.display = 'none';
+document.getElementById('draw-btn').style.display = 'none';
+document.getElementById('fold-btn').style.display = 'none';
+
+let dealer_sum = 0;
+let player_chips = 150;
+let wager = 0;
+
+document.getElementById('chips').innerHTML = "Your Chips: $" + player_chips;
+
 let cards = [
-	"Ah", "Ad", "Ac", "As",
-	"2h", "2d", "2c", "2s",
-	"3h", "3d", "3c", "3s",
-	"4h", "4d", "4c", "4s",
-	"5h", "5d", "5c", "5s",
-	"6h", "6d", "6c", "6s",
-	"7h", "7d", "7c", "7s",
-	"8h", "8d", "8c", "8s",
-	"9h", "9d", "9c", "9s",
-	"10h", "10d", "10c", "10s",
-	"Jh", "Jd", "Jc", "Js",
-	"Qh", "Qd", "Qc", "Qs",
-	"Kh", "Kd", "Kc", "Ks"
-];
-let dealer_cards = [
 	"Ah", "Ad", "Ac", "As",
 	"2h", "2d", "2c", "2s",
 	"3h", "3d", "3c", "3s",
@@ -38,7 +31,7 @@ let dealer_cards = [
 ];
 
 let card_values = {
-	"Ah": 1, "Ad": 1, "Ac": 1, "As": 1,
+	"Ah": 11, "Ad": 11, "Ac": 11, "As": 11,
 	"2h": 2, "2d": 2, "2c": 2, "2s": 2,
 	"3h": 3, "3d": 3, "3c": 3, "3s": 3,
 	"4h": 4, "4d": 4, "4c": 4, "4s": 4,
@@ -54,23 +47,33 @@ let card_values = {
 }
 
 function startGame() {
-	game(0);
+	document.getElementById('wager').style.display = 'inline-block';
+	document.getElementById('bet-btn').style.display = 'inline-block';
+	document.getElementById('bet').style.display = 'none';
+	document.getElementById('draw-btn').style.display = 'inline-block';
+	document.getElementById('fold-btn').style.display = 'inline-block';
+	document.getElementById('game-btn').style.display = 'none';
+	document.getElementById('restart-btn').style.display = 'none';
+	document.getElementById('chips').innerHTML = "Your Chips: $" + player_chips;
+	game_sum.innerHTML = 0;
+	card_deck.innerHTML = '';
+	gameOver = false;
+	bet();
+	game();
 }
-function game(num) {
-	if (parseInt(game_sum.innerHTML) + parseInt(num) < 21) {
+function game() {
+	if (parseInt(game_sum.innerHTML) < 21) {
 		message = "Do you want to draw a new card?"
 	}
-	else if (parseInt(game_sum.innerHTML) + parseInt(num) > 21) {
-		if (game_sum.innerHTML >= dealer_sum) {
-			message = "You Win!!! You've got Blackjack";
-		}
-		else {
-			message = "You Lose!!! You're out of the game!"
-		}
+	else if (parseInt(game_sum.innerHTML) > 21) {
+		message = "You Lose!!! You're out of the game!"
 		gameOver = true;
+		player_chips = parseInt(player_chips) - parseInt(wager);
 	}
 	else {
 		message = "You Win!!! You've got Blackjack";
+		gameOver = true;
+		player_chips = parseInt(player_chips) + parseInt(wager);
 	}
 
 	display_message.innerHTML = message;
@@ -78,7 +81,10 @@ function game(num) {
 		document.getElementById('draw-btn').style.display = 'none';
 		document.getElementById('game-btn').style.display = 'none';
 		document.getElementById('fold-btn').style.display = 'none';
-		document.getElementById('restart-btn').style.display = 'block';
+		document.getElementById('restart-btn').style.display = 'inline-block';
+		document.getElementById('wager').style.display = 'none';
+		document.getElementById('bet-btn').style.display = 'none';
+		document.getElementById('bet').style.display = 'none';
 		return;
 	}
 
@@ -88,54 +94,45 @@ function draw_card() {
 	let selection = cards[Math.floor(Math.random() * cards.length)];
 	cards.splice(cards.indexOf(selection), 1);
 	card_deck.innerHTML += ' ' + selection;
-	get_dealers_card();
-	if (parseInt(game_sum.innerHTML) + parseInt(card_values[selection]) > 21) {
-		game(card_values[selection]);
-		card_deck.innerHTML += ' Would have - ' + selection;
-		return;
-	}
 	game_sum.innerHTML = parseInt(game_sum.innerHTML) + parseInt(card_values[selection]);
-	game(card_values[selection]);
+	game();
 }
 
 function fold() {
-	get_dealers_card();
 	document.getElementById('draw-btn').style.display = 'none';
 	document.getElementById('game-btn').style.display = 'none';
 	document.getElementById('fold-btn').style.display = 'none';
-	document.getElementById('restart-btn').style.display = 'block';
+	document.getElementById('restart-btn').style.display = 'inline-block';
+
+	dealers_call();
+
 	if (parseInt(game_sum.innerHTML) >= parseInt(dealer_sum)) {
 		message = "You Win!!! You've got Blackjack";
+		player_chips = parseInt(player_chips) + parseInt(wager);
 	}
 	else {
 		message = "You lose!!! You're out of the game!";
+		player_chips = parseInt(player_chips) - parseInt(wager);
 	}
-
+	document.getElementById('chips').innerHTML = "Your Chips: $" + player_chips;
+	document.getElementById('dealer').innerHTML = "Dealer's Sum: " + dealer_sum;
 	display_message.innerHTML = message;
 }
 
-function get_dealers_card() {
-	if (dealer_sum >= 21) {
-		return;
-	}
-	let selection = dealer_cards[Math.floor(Math.random() * dealer_cards.length)];
-	if (dealer_sum + card_values[selection] > 21) {
-		document.querySelector('#dealer-deck').innerHTML += ' Would have - ' + selection;
-		return;
-	}
-	dealer_cards.splice(dealer_cards.indexOf(selection), 1);
-	dealer_sum += card_values[selection];
-	document.querySelector('#dealer-deck').innerHTML += ' ' + selection;
-	document.querySelector('#dealer-sum').innerHTML = parseInt(dealer_sum);
+function bet() {
+	let num = document.getElementById('wager').value
+	wager = num;
+	document.getElementById('wager').style.display = 'none';
+	document.getElementById('bet-btn').style.display = 'none';
+	document.getElementById('bet').style.display = 'inline-block';
+	document.getElementById('bet').innerHTML = 'Your Bet: $' + num;
+}
+
+function dealers_call() {
+	dealer_sum = Math.floor(Math.random() * (21 - 10 + 1)) + 10;
 }
 
 function restartGame() {
-	dealer_sum = 0;
-	display_message = document.querySelector('#display-message')
-	card = 0;
-	message = '';
-	gameOver = false;
-	document.getElementById('restart-btn').style.display = 'none';
 	cards = [
 		"Ah", "Ad", "Ac", "As",
 		"2h", "2d", "2c", "2s",
@@ -151,29 +148,7 @@ function restartGame() {
 		"Qh", "Qd", "Qc", "Qs",
 		"Kh", "Kd", "Kc", "Ks"
 	];
-	dealer_cards = [
-		"Ah", "Ad", "Ac", "As",
-		"2h", "2d", "2c", "2s",
-		"3h", "3d", "3c", "3s",
-		"4h", "4d", "4c", "4s",
-		"5h", "5d", "5c", "5s",
-		"6h", "6d", "6c", "6s",
-		"7h", "7d", "7c", "7s",
-		"8h", "8d", "8c", "8s",
-		"9h", "9d", "9c", "9s",
-		"10h", "10d", "10c", "10s",
-		"Jh", "Jd", "Jc", "Js",
-		"Qh", "Qd", "Qc", "Qs",
-		"Kh", "Kd", "Kc", "Ks"
-	];
 
-	document.getElementById('draw-btn').style.display = 'block';
-	document.getElementById('game-btn').style.display = 'block';
-	document.getElementById('fold-btn').style.display = 'block';
-	document.getElementById('restart-btn').style.display = 'none';
-	document.querySelector('#card-deck').innerHTML = '';
-	document.querySelector('#game-sum').innerHTML = 0;
-	document.querySelector('#dealer-deck').innerHTML = '';
-	document.querySelector('#dealer-sum').innerHTML = '';
-	dealer_sum = 0;
+	document.getElementById('dealer').innerHTML = '';
+	startGame()
 }
